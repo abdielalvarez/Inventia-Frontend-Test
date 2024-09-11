0fbcdbc9684fa5c3713e90b7fe888401d64d3b4b
Abdiel Alvarez Rojas <abdielalvarez@icloud.com>
2024-09-10 21:16:51 -0600

    first section from banking sections

--- src/app/success-stories/bbva/page.js
+++ src/app/success-stories/bbva/page.js
@@ -1,6 +1,10 @@
 'use client'
+import Brief from "@/block-pages/SuccessStories/BBVA/Brief";
+import ContactUs from "@/block-pages/SuccessStories/BBVA/ContactUs";
 import Cover from "@/block-pages/SuccessStories/BBVA/Cover";
 import Main from "@/block-pages/SuccessStories/BBVA/Main";
+import Results from "@/block-pages/SuccessStories/BBVA/Results";
+import Success from "@/block-pages/SuccessStories/BBVA/Success";
 import React from "react";
 
 export default function SuccessStoriesBBVA() {
@@ -8,6 +12,10 @@ export default function SuccessStoriesBBVA() {
     <main>
       <Main />
       <Cover />
+      <Brief />
+      <Results />
+      <ContactUs />
+      <Success />
     </main>
   );
 }
---
+++ src/block-pages/SuccessStories/BBVA/Results/index.js
@@ -0,0 +1,74 @@
+import { useApiContext } from "@/context/wrappers/ContextProvider";
+import styles from "../../../../styles/block-pages/success-stories/bbva/results.module.css"
+import useResponsive from "@/hooks/useResponsive";
+import Text from "@/components/Text";
+import Image from "next/image";
+
+const Results = () => {
+
+    const { t } = useApiContext()
+    const isResponsive = useResponsive(768)
+
+    const titleText = {
+        text: t('success-stories.banking-bbva.block3.title'),
+        tag: "p",
+        font: "poppinsSemibold",
+        size: isResponsive ? 15 : 40,
+        color: 'blueBase',
+    }
+
+    const description1Text = [
+        {
+            text: t('success-stories.banking-bbva.block3.text1'),
+            tag: "p",
+            font: "poppinsLight",
+            size: isResponsive ? 11 : 23,
+            color: 'blackBase',
+        },
+        {
+            text: t('success-stories.banking-bbva.block3.text2'),
+            tag: "p",
+            font: "poppinsLight",
+            size: isResponsive ? 11 : 23,
+            color: 'blackBase',
+        },
+    ]
+
+    const description2Text = [
+        {
+            text: t('success-stories.banking-bbva.block3.text3'),
+            tag: "p",
+            font: "poppinsLight",
+            size: isResponsive ? 11 : 23,
+            color: 'blackBase',
+        },
+    ]
+
+    return (
+        <div className={styles.background}>
+            <div className={styles.wrapper}>
+                <Image
+                    src="/images/success-stories/banking-bbva/results/cellphone.jpg"
+                    alt="BBVA"
+                    width={isResponsive ? 175 : 501}
+                    height={isResponsive ? 229 : 655}
+                    priority
+                    className={styles.img}
+                />
+                <div>
+                    <div className={styles.titleContainer}>
+                        <Text text={titleText} />
+                    </div>
+                    <div className={styles.description1Container}>
+                        <Text texts={description1Text} />
+                    </div>
+                    <div className={styles.description2Container}>
+                        <Text texts={description2Text} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Results
