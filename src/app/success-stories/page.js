e2497ea25d4735d6818e07ae33f1be5aa0b059bc
Abdiel Alvarez Rojas <abdielalvarez@icloud.com>
2024-11-20 21:57:29 -0600

    text and testimonials

--- src/app/success-stories/page.js
+++ src/app/success-stories/page.js
@@ -22,7 +22,7 @@ export default function SuccessStories() {
         <Projects />
         <Options />
         <Description />
-        <Carousel />
+        {/* <Carousel /> */}
         <ContactUs />
       </main>
     </>
